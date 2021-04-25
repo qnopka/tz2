4bc6b9beca93224784f6e0af9cbe1dcc5b4347e5
Попович Екатерина <71285738+qnopka@users.noreply.github.com>
2021-04-25 14:08:19 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+function applyTheme(theme) {
+  document.body.classList.remove("theme-light", "theme-dark", "theme-grey");
+  document.body.classList.add(`theme-${theme}`);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".theme").addEventListener("change", function() {
+    applyTheme(this.value);
+  });
+});
+
+
+let body = document.querySelector("body"),
+    goLight = function() {
+        body.classList.add("theme-light");
+    },
+    goDark = function() {
+      body.className = "";
+      body.classList.add("theme-dark");
+    },
+    goGrey = function() {
+      body.className = "";
+      body.classList.add("theme-grey");
+    }
+
+document.querySelector(".theme-light").addEventListener("click", goLight, false);
+document.querySelector(".theme-dark").addEventListener("click", goDark, false);
+document.querySelector(".theme-grey").addEventListener("click", goGrey, false);
+
