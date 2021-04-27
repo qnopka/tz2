60378e8a75d8d190d918261856f8540e99a46e3b
Попович Екатерина <71285738+qnopka@users.noreply.github.com>
2021-04-27 09:54:22 +0300

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,23 +29,3 @@ goDark = function() {
 document.querySelector(".theme-light").addEventListener("click", goLight, false);
 document.querySelector(".theme-dark").addEventListener("click", goDark, false);
 document.querySelector(".theme-grey").addEventListener("click", goGrey, false);
-
-
-$(function(){
-    $('.minimized').click(function(event) {
-        var i_path = $(this).attr('src');
-        $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
-        $('#magnify').css({
-            left: ($(document).width() - $('#magnify').outerWidth())/2,
-            top: ($(window).height() - $('#magnify').outerHeight())/2
-        });
-        $('#overlay, #magnify').fadeIn('fast');
-    });
-
-    $('body').on('click', '#close-popup, #overlay', function(event) {
-        event.preventDefault();
-        $('#overlay, #magnify').fadeOut('fast', function() {
-            $('#close-popup, #magnify, #overlay').remove();
-        });
-    });
-});
